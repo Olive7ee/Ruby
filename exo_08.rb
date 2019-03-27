2d04e0e7c52dc2c8a40287345b6878a7b2632ec9
Olive7e <olivier.thd@gmail.com>
2019-03-27 17:25:17 +0100

    Journée de jeudi

---
+++ exo_08.rb
@@ -0,0 +1,4 @@
+puts "Quel est votre prénom ?"
+user_name = gets.chomp
+
+puts "Salut "+ user_name
---
+++ exo_09.rb
@@ -0,0 +1,8 @@
+puts "Quel est votre prénom ?"
+user_nickmane = gets.chomp
+
+puts "Quel est votre prénom"
+user_name  =  gets.chomp
+
+puts "Salut "+ user_nickmane + " " + user_name
+
